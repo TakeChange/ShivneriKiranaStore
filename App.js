40c3c9c5e49f6438c87850f5d174dd377ba4e16c
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-01-24 16:26:55 +0530

    push changes

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import DonateMapScreen from './src/screen/DonateMapScreen'
+import DonateScreen from './src/screen/DonateScreen'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+   <DonateScreen/>
   )
 }
 
