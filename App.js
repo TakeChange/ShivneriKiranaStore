7acfa584c0c6ed85ccb07fef0524a4aa876955bd
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-01-24 17:32:38 +0530

    submit screen changes

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import DonateMapScreen from './src/screen/DonateMapScreen'
 import DonateScreen from './src/screen/DonateScreen'
+import Submit from './src/screen/Submit'
 
 const App = () => {
   return (
-   <DonateScreen/>
+  <Submit/>
   )
 }
 
