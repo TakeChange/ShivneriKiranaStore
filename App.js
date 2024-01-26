aa0ad2c6958b004fe164a05e6fc53665970e5a17
manishashirsath2001 <manishashirsath0204@gmail.com>
2024-01-26 16:25:50 +0530

    pull changes

--- App.js
+++ App.js
@@ -3,21 +3,14 @@ import React from 'react'
 import DonateMapScreen from './src/screen/DonateMapScreen'
 import DonateScreen from './src/screen/DonateScreen'
 import Submit from './src/screen/Submit'
-<<<<<<< HEAD
-
-const App = () => {
-  return (
-  <Submit/>
-=======
-
+import PinsScreen from './src/screen/PinsScreen'
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+    <Text>App</Text>
     </View>
     
->>>>>>> 291bdc06aaadf72209f381498660c480caf50b0b
   )
 }
 
