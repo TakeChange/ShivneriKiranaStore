298f55692b50036be5d875ff9a11c860eefab77b
manishashirsath2001 <manishashirsath0204@gmail.com>
2024-01-27 15:33:15 +0530

    pull changes

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import AppNavigation from './src/navigation/AppNavigation'
+import PinsScreen from './src/screen/PinsScreen'
 
 
 
 const App = () => {
   return (
     <AppNavigation/>
-      
   )
 }
 
