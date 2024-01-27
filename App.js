4c57de2f189b10ba378535cb360afaed70a0e440
monika7416 <monikajadhav7416@gmail.com>
2024-01-27 10:40:40 +0530

    pull changes

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import DonateMapScreen from './src/screen/DonateMapScreen'
 import DonateScreen from './src/screen/DonateScreen'
 import Submit from './src/screen/Submit'
 import PinsScreen from './src/screen/PinsScreen'
+import Receive from './src/screen/Receive'
 
 const App = () => {
   return (
-    <View>
-    <Text>App</Text>
-    </View>
-  
+    // <View>
+    // <Text>App</Text>
+    // </View>
+  <Receive/>
     
   )
 }
