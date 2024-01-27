03c58bb66e8ca9356466ed95257399ffdfaabdda
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-01-27 10:26:41 +0530

    push updated code

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-import DonateMapScreen from './src/screen/DonateMapScreen'
-import DonateScreen from './src/screen/DonateScreen'
-import Submit from './src/screen/Submit'
-import PinsScreen from './src/screen/PinsScreen'
+import AppNavigation from './src/navigation/AppNavigation'
 
 const App = () => {
   return (
-    <View>
-    <Text>App</Text>
-    </View>
-  
-    
+    <AppNavigation/>
   )
 }
 
---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from '../screen/SplashScreen';
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
